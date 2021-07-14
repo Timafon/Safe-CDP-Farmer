f51dd014f2d4b2c2edf0f6d3e3c35814ef4052e2
Artem Zvyagin <zvyaginartems@gmail.com>
2021-07-14 17:18:15 +0300

    aave.borrow(MATIC) [1]

--- packages/react-app/src/views/SafeFarmer.jsx
+++ packages/react-app/src/views/SafeFarmer.jsx
@@ -10,7 +10,234 @@ const wMaticAddress = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
 const USDTAddress = "0xc2132D05D31c914a87C6611C10748AEb04B58e8F"; // aToken
 const sUSDTAddress = "0xe590cfca10e81FeD9B0e4496381f02256f5d2f61"; // stable debt
 const vUSDTAddress = "0x8038857FD47108A07d1f6Bf652ef1cBeC279A2f3"; // variable debt
+const aUSDTAddress = "0x60D55F02A771d515e077c9C2403a1ef324885CeC"; // get all tokens
 const SafeCDPFarmerAddress = "0x5c1fD33A842F05c0F4f702928fA1ED4A8bd05020";
+const ProtocolDataProviderAddress = "0x7551b5D2763519d4e37e8B81929D336De671d46d";
+const ProtocolDataProviderABI = [
+  {
+    inputs: [{ internalType: "contract ILendingPoolAddressesProvider", name: "addressesProvider", type: "address" }],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "ADDRESSES_PROVIDER",
+    outputs: [{ internalType: "contract ILendingPoolAddressesProvider", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getAllATokens",
+    outputs: [
+      {
+        components: [
+          { internalType: "string", name: "symbol", type: "string" },
+          { internalType: "address", name: "tokenAddress", type: "address" },
+        ],
+        internalType: "struct AaveProtocolDataProvider.TokenData[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getAllReservesTokens",
+    outputs: [
+      {
+        components: [
+          { internalType: "string", name: "symbol", type: "string" },
+          { internalType: "address", name: "tokenAddress", type: "address" },
+        ],
+        internalType: "struct AaveProtocolDataProvider.TokenData[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "asset", type: "address" }],
+    name: "getReserveConfigurationData",
+    outputs: [
+      { internalType: "uint256", name: "decimals", type: "uint256" },
+      { internalType: "uint256", name: "ltv", type: "uint256" },
+      { internalType: "uint256", name: "liquidationThreshold", type: "uint256" },
+      { internalType: "uint256", name: "liquidationBonus", type: "uint256" },
+      { internalType: "uint256", name: "reserveFactor", type: "uint256" },
+      { internalType: "bool", name: "usageAsCollateralEnabled", type: "bool" },
+      { internalType: "bool", name: "borrowingEnabled", type: "bool" },
+      { internalType: "bool", name: "stableBorrowRateEnabled", type: "bool" },
+      { internalType: "bool", name: "isActive", type: "bool" },
+      { internalType: "bool", name: "isFrozen", type: "bool" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "asset", type: "address" }],
+    name: "getReserveData",
+    outputs: [
+      { internalType: "uint256", name: "availableLiquidity", type: "uint256" },
+      { internalType: "uint256", name: "totalStableDebt", type: "uint256" },
+      { internalType: "uint256", name: "totalVariableDebt", type: "uint256" },
+      { internalType: "uint256", name: "liquidityRate", type: "uint256" },
+      { internalType: "uint256", name: "variableBorrowRate", type: "uint256" },
+      { internalType: "uint256", name: "stableBorrowRate", type: "uint256" },
+      { internalType: "uint256", name: "averageStableBorrowRate", type: "uint256" },
+      { internalType: "uint256", name: "liquidityIndex", type: "uint256" },
+      { internalType: "uint256", name: "variableBorrowIndex", type: "uint256" },
+      { internalType: "uint40", name: "lastUpdateTimestamp", type: "uint40" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "asset", type: "address" }],
+    name: "getReserveTokensAddresses",
+    outputs: [
+      { internalType: "address", name: "aTokenAddress", type: "address" },
+      { internalType: "address", name: "stableDebtTokenAddress", type: "address" },
+      { internalType: "address", name: "variableDebtTokenAddress", type: "address" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "asset", type: "address" },
+      { internalType: "address", name: "user", type: "address" },
+    ],
+    name: "getUserReserveData",
+    outputs: [
+      { internalType: "uint256", name: "currentATokenBalance", type: "uint256" },
+      { internalType: "uint256", name: "currentStableDebt", type: "uint256" },
+      { internalType: "uint256", name: "currentVariableDebt", type: "uint256" },
+      { internalType: "uint256", name: "principalStableDebt", type: "uint256" },
+      { internalType: "uint256", name: "scaledVariableDebt", type: "uint256" },
+      { internalType: "uint256", name: "stableBorrowRate", type: "uint256" },
+      { internalType: "uint256", name: "liquidityRate", type: "uint256" },
+      { internalType: "uint40", name: "stableRateLastUpdated", type: "uint40" },
+      { internalType: "bool", name: "usageAsCollateralEnabled", type: "bool" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+const PriceOracleAddress = "0x0229f777b0fab107f9591a41d5f02e4e98db6f2d";
+const PriceOracleABI = [
+  {
+    inputs: [
+      { internalType: "address[]", name: "assets", type: "address[]" },
+      { internalType: "address[]", name: "sources", type: "address[]" },
+      { internalType: "address", name: "fallbackOracle", type: "address" },
+      { internalType: "address", name: "weth", type: "address" },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "asset", type: "address" },
+      { indexed: true, internalType: "address", name: "source", type: "address" },
+    ],
+    name: "AssetSourceUpdated",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [{ indexed: true, internalType: "address", name: "fallbackOracle", type: "address" }],
+    name: "FallbackOracleUpdated",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "previousOwner", type: "address" },
+      { indexed: true, internalType: "address", name: "newOwner", type: "address" },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [{ indexed: true, internalType: "address", name: "weth", type: "address" }],
+    name: "WethSet",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "WETH",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "asset", type: "address" }],
+    name: "getAssetPrice",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address[]", name: "assets", type: "address[]" }],
+    name: "getAssetsPrices",
+    outputs: [{ internalType: "uint256[]", name: "", type: "uint256[]" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getFallbackOracle",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "asset", type: "address" }],
+    name: "getSourceOfAsset",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  { inputs: [], name: "renounceOwnership", outputs: [], stateMutability: "nonpayable", type: "function" },
+  {
+    inputs: [
+      { internalType: "address[]", name: "assets", type: "address[]" },
+      { internalType: "address[]", name: "sources", type: "address[]" },
+    ],
+    name: "setAssetSources",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "fallbackOracle", type: "address" }],
+    name: "setFallbackOracle",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
 const AaveAddress = "0x8dFf5E27EA6b7AC08EbFdf9eB090F32ee9a30fcf";
 const AaveABI = [
   {
@@ -501,12 +728,19 @@ const AaveProxyABI = [
     type: "function",
   },
 ];
-// TODO повесить прослушку Event чтобы обновлять фронт
+// TODO добавить прослушку Event чтобы обновлять фронт
 export function SafeFarmer({ address, signer }) {
   const [userAave, setUserAave] = React.useState();
   const aaveProxyContract = React.useMemo(() => {
     return new ethers.Contract(AaveAddress, AaveProxyABI, signer);
   }, [signer]);
+  const aaveOracleContract = React.useMemo(() => {
+    return new ethers.Contract(PriceOracleAddress, PriceOracleABI, signer);
+  }, [signer]);
+  const ProtocolDataProviderContract = React.useMemo(() => {
+    return new ethers.Contract(ProtocolDataProviderAddress, ProtocolDataProviderABI, signer);
+  }, [signer]);
+  // console.log("logs aaveOracleContract: ", aaveOracleContract);
   React.useEffect(() => {
     async function getUserAccountData() {
       console.log("[MYLOGS] address: ", address);
@@ -529,15 +763,58 @@ export function SafeFarmer({ address, signer }) {
     async function rebalance(targetBorrowAmount) {
       // https://docs.aave.com/developers/the-core-protocol/lendingpool#borrow
       // function borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf)
-      const asset = wMaticAddress; // [address] address of the underlying asset
-      const amount = targetBorrowAmount; // [uint256]
+      // const asset = wMaticAddress; // [address] address of the underlying asset
+      const asset = USDTAddress; // vUSDTAddress; // [address] address of the underlying asset
+      // const forBigNumber = ethers.utils.formatUnits(targetBorrowAmount);
+      // console.log("forBigNumber: ", forBigNumber);
+
+      console.log("logs ProtocolDataProviderContract: ", ProtocolDataProviderContract);
+      const allTokens = await ProtocolDataProviderContract.getAllReservesTokens(); // getAllATokens();
+      console.log("logs reserves allTokens: ", allTokens);
+
+      let usdtEth = 0;
+      const asd = await aaveOracleContract.getAssetPrice(asset);
+      console.log("logs asd: ", asd);
+      try {
+        usdtEth = await aaveOracleContract.getAssetPrice(asset);
+      } catch (err) {
+        console.log("logs aaveOracleContract.getAssetPrice(asset): ", err);
+      }
+
+      console.log("logs usdtEth: ", usdtEth);
+      // const amount = ethers.BigNumber.from(ethers.utils.formatUnits(targetBorrowAmount)); // [uint256]
+      const amount = ethers.BigNumber.from(targetBorrowAmount); // [uint256]
+      // console.log(
+      //   "logs ethers.BigNumber.from(ethers.utils.formatUnits(targetBorrowAmount)): ",
+      //   ethers.BigNumber.from(ethers.utils.formatUnits(targetBorrowAmount)),
+      // );
+      const toNumberAmount = amount.toNumber();
+      console.log("logs toNumberAmount: ", toNumberAmount);
+      // console.log("logs ethers.utils.formatUnits(targetBorrowAmount): ",
+      // ethers.utils.formatUnits(targetBorrowAmount));
+      // console.log(
+      //   "logs ethers.utils.formatUnits(targetBorrowAmount): ",
+      //   ethers.BigNumber.from(ethers.utils.formatUnits(targetBorrowAmount)),
+      // );
       // amount to be borrowed, expressed in wei
       // units
       const interestRateMode = ethers.BigNumber.from(2); // [uint256] the type of borrow debt. Stable: 1, Variable: 2
       const referralCode = 0; // [uint16] referral code for our referral program. Use 0 for
       // no referral code.
+      console.log("logs onBehalfOf: ", address);
       const onBehalfOf = address; // [address] address of user who will incur the debt
-      // debugger;
+      // string public constant VL_RESERVE_FROZEN = '3'; // 'Action cannot be performed because the reserve is frozen'
+      // 3 Currency not borrowed
+      const borrowData = {
+        asset,
+        // targetBorrowAmount,
+        amount,
+        interestRateMode,
+        referralCode,
+        onBehalfOf,
+      };
+      console.log("logs borrow data: ", borrowData);
+
       try {
         const res = await aaveProxyContract.borrow(asset, amount, interestRateMode, referralCode, onBehalfOf);
         console.log("logs rebalance res: ", res);
@@ -548,7 +825,12 @@ export function SafeFarmer({ address, signer }) {
 
     if (userAave) {
       const currentHF = ethers.utils.formatUnits(userAave.healthFactor);
-      const isRange = currentHF >= TARGET_HF - 0.5 && currentHF <= TARGET_HF + 0.5;
+      console.log("logs HFS: ", {
+        currentHF,
+        TARGET_HF,
+        numberCHF: Number(currentHF),
+      });
+      const isRange = Number(currentHF) >= TARGET_HF - 0.05 && Number(currentHF) <= TARGET_HF + 0.05;
 
       if (isRange)
         return {
@@ -638,7 +920,11 @@ export function SafeFarmer({ address, signer }) {
             {userAave && rebalanceModule.rise
               ? "Need to borrow money on Aave and deposit on Curve: "
               : "Need withdraw money on Curve and repay on Aave: "}
-            {userAave ? ethers.utils.formatUnits(rebalanceModule.targetBorrowAmount) : "loading..."}
+            {userAave
+              ? typeof rebalanceModule.targetBorrowAmount !== "string"
+                ? ethers.utils.formatUnits(rebalanceModule.targetBorrowAmount)
+                : rebalanceModule.targetBorrowAmount
+              : "loading..."}
           </Typography>
         </Row>
 
